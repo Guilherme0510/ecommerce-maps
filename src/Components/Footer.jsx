e1a0e3e6319b9ca315819dfe7a3c889ffa305189
Guilherme Silva <guissilval005@gmail.com>
2024-10-25 16:36:54 -0300

    Fix the footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className="px-8 mt-12 flex flex-col gap-5 mb-4">
-      <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr_1fr] items-top justify-center">
-        <div className="flex flex-col">
-          <img className="w-56" src={images.logo} alt="Logo da Pastelaria" />
-          <p className="text-base w-1/2">
+    <div className="px-8 mt-12 flex flex-col gap-5 mb-4" data-aos='fade-down' data-aos-duration='500'>
+      <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr_1fr] items-top justify-center gap-5">
+        <div className="flex flex-col md:items-start items-center" data-aos='fade-right' data-aos-duration='500' data-aos-delay='500'>
+          <img className="w-32 md:w-56" src={images.logo} alt="Logo da Pastelaria" />
+          <p className="text-base w-3/4 md:text-left text-center">
             Na nossa pastelaria, cada mordida é uma explosão de sabor, feita com
             ingredientes frescos e receitas que aquecem o coração.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-center">
+        <div className="flex flex-col gap-4 text-center" data-aos='fade-down' data-aos-duration='500' data-aos-delay='500'>
           <h1 className="font-bold text-xl">Companhia</h1>
           <div className="flex flex-col gap-3">
             <Link to='/' className=" hover:underline">Inicio</Link>
@@ -22,7 +22,7 @@ const Footer = () => {
             <Link to='/contato' className=" hover:underline">Contato</Link>
           </div>
         </div>
-        <div className="flex flex-col gap-4 text-center">
+        <div className="flex flex-col gap-4 text-center" data-aos='fade-left' data-aos-duration='500' data-aos-delay='500'>
           <h1 className="font-bold text-xl">Contate já</h1>
           <div className="flex flex-col gap-3">
             <Link to='tel:70970707070' className=" hover:underline">(70) 97070-7070</Link>
@@ -31,7 +31,7 @@ const Footer = () => {
         </div>
       </div>
       <hr className="w-4/5 mx-auto" />
-      <div>
+      <div data-aos='fade-up' data-aos-duration='500' data-aos-delay='500'>
         <p className="text-center">
           Copyright 2024@ Teste.com - Todos os Direitos Reservados
         </p>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ import Avaliacao from '../Components/Avaliacao'
 
 const Home = () => {
   return (
-    <div className='px-4 w-5/5 md:w-4/5 mx-auto flex flex-col gap-12'>
+    <div className='px-8 w-5/5 md:w-4/5 mx-auto flex flex-col gap-12'>
       <Banner />
       <Destaques />
       <Avaliacao />
